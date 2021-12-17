6845714cf32e614c4bc3a4279c28744bd2f45ed6
Andrew <andrew.ndrw1@gmail.com>
2021-12-17 20:22:33 +0300

    multiple return

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,32 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([])
+  const getUsers = async() => {
+    const response = await fetch(url)
+    const users = await response.json()
+    setUsers(users)
+    // must be carful beacause you can get endless loop by constantly trigerring useState which triggers useEffect
+  }
+  useState(() => {
+    getUsers()
+  }, [])
+  // usestate cannot use async await, and async await always return promise
+  return <React.Fragment>
+    <h3>GitHub Users</h3>
+    <ul className='users'>
+    {users.map((user) => {
+      const {id, login, avatar_url, html_url} = user
+      return <li key={id}>
+        <img src={avatar_url} alt={login} />
+        <div>
+          <h4>{login}</h4>
+          <a href={html_url}>Profile</a>
+        </div>
+      </li>
+})}
+    </ul>
+  </React.Fragment>
 };
 
 export default UseEffectFetchData;
